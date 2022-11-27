31356658977618289cb952916e37c7e307d4938e
CookieMC337 <julienmichell2003@gmail.com>
2022-11-27 14:52:00 +0100

    Updated Response Types

--- src/WebStatusChecker.php
+++ src/WebStatusChecker.php
@@ -9,125 +9,125 @@ use GuzzleHttp\Exception\ServerException;
 
 class WebStatusChecker
 {
-	/**
-	 * @var \GuzzleHttp\Client
-	 */
-	protected $client;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
 
-	/**  
-	 * @var int 
-	 */
-	protected $responseCode;
+    /**
+     * @var int
+     */
+    protected $responseCode;
 
-	/** 
-	 * @var int 
-	 */
-	protected $timeout = 5;
+    /**
+     * @var int
+     */
+    protected $timeout = 5;
 
-	/** 
-	 * @var bool 
-	 */
-	protected $allowRedirects = true;
+    /**
+     * @var bool
+     */
+    protected $allowRedirects = true;
 
-	/**
-	 * @var arr
-	 */
-	protected $dns;
+    /**
+     * @var arr
+     */
+    protected $dns;
 
-	public function __construct()
-	{
-		$this->client = new Client([
-			'timeout' => $this->timeout,
-			'allow_redirects' => $this->allowRedirects,
-		]);
-	}
+    public function __construct()
+    {
+        $this->client = new Client([
+            'timeout' => $this->timeout,
+            'allow_redirects' => $this->allowRedirects,
+        ]);
+    }
 
-	public function check($url)
-	{
-		try {
-			$response = $this->client->get($url);
-			$this->responseCode = $response->getStatusCode();
-		} catch(ClientException $e) {
-			$response = $e->getResponse();
-			$this->responseCode = $response->getStatusCode();
-		} catch(ConnectException $e) {
-			$this->responseCode = $e->getCode();
-		} catch(ServerException $e) {
-			$this->responseCode = $e->getCode();
-		}
+    public function check($url)
+    {
+        try {
+            $response = $this->client->get($url);
+            $this->responseCode = $response->getStatusCode();
+        } catch(ClientException $e) {
+            $response = $e->getResponse();
+            $this->responseCode = $response->getStatusCode();
+        } catch(ConnectException $e) {
+            $this->responseCode = $e->getCode();
+        } catch(ServerException $e) {
+            $this->responseCode = $e->getCode();
+        }
 
-		return $this->responseCode;
-	}
+        return $this->responseCode;
+    }
 
-	public function checkA($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_A));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['ip'] == $val;
-		});
-		return $filtered->count() > 0;
-	}
+    public function checkA($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_A));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['ip'] == $val;
+        });
+        return $filtered->count() > 0;
+    }
 
-	public function checkCname($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_CNAME));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
+    public function checkCname($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_CNAME));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
 
-	public function checkNameserver($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_NS));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
-	
-	public function checkSRV($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_SRV));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
-	
-        public function checkPTR($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_PTR));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
-	
-        public function checkSOA($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_SOA));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
-	
-        public function checkTXT($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_TXT));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
-	
-        public function checkAAAA($host, $val)
-	{
-		$this->dns = collect(dns_get_record($host, DNS_AAAA));
-		$filtered = $this->dns->filter(function($item, $key) use($val) {
-			return $item['target'] === $val;
-		});
-		return $filtered->count() > 0;
-	}
+    public function checkNameserver($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_NS));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
+
+    public function checkSRV($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_SRV));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
+
+    public function checkPTR($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_PTR));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
+
+    public function checkSOA($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_SOA));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
+
+    public function checkTXT($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_TXT));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
+
+    public function checkAAAA($host, $val): bool
+    {
+        $this->dns = collect(dns_get_record($host, DNS_AAAA));
+        $filtered = $this->dns->filter(function($item, $key) use($val) {
+            return $item['target'] === $val;
+        });
+        return $filtered->count() > 0;
+    }
 }
